0ff0751f8ae7b638f3cee6df9493fabffa09b458
YiYing He <yiying@secondstate.io>
2021-10-27 00:37:15 +0800

    [Tools] Handle the return types in reactor mode.
    Signed-off-by: YiYing He <yiying@secondstate.io>

--- tools/wasmedge/wasmedger.cpp
+++ tools/wasmedge/wasmedger.cpp
@@ -242,19 +242,19 @@ int main(int Argc, const char *Argv[]) {
 
     if (auto Result = VM.execute(FuncName, FuncArgs, FuncArgTypes)) {
       /// Print results.
-      for (size_t I = 0; I < FuncType.getReturnTypes().size(); ++I) {
-        switch (FuncType.getReturnTypes()[I]) {
+      for (size_t I = 0; I < (*Result).first.size(); ++I) {
+        switch ((*Result).second[I]) {
         case WasmEdge::ValType::I32:
-          std::cout << (*Result)[I].get<uint32_t>() << '\n';
+          std::cout << (*Result).first[I].get<uint32_t>() << '\n';
           break;
         case WasmEdge::ValType::I64:
-          std::cout << (*Result)[I].get<uint64_t>() << '\n';
+          std::cout << (*Result).first[I].get<uint64_t>() << '\n';
           break;
         case WasmEdge::ValType::F32:
-          std::cout << (*Result)[I].get<float>() << '\n';
+          std::cout << (*Result).first[I].get<float>() << '\n';
           break;
         case WasmEdge::ValType::F64:
-          std::cout << (*Result)[I].get<double>() << '\n';
+          std::cout << (*Result).first[I].get<double>() << '\n';
           break;
         /// TODO: FuncRef and ExternRef
         default:
